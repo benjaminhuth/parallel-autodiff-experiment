3be4b41ff1cc378276d495f72647fc4469582bb7
Benjamin Huth <Benjamin-Huth@t-online.de>
2020-07-22 16:14:47 +0200

    Replaced selfmade type with Eigen::Array, some cleanup.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <Eigen/Core>
 #include <autodiff/forward.hpp>
-#include <autodiff/forward/eigen.hpp>
-#include "grad_array.hpp"
 
 /******************************************************
  * 
@@ -29,7 +27,7 @@
 //                                    1-dim value      2-dim grad, this type has all necessary overloads
 //                                         |            |
 //                                         V            V
-using my_dual2 = autodiff::forward::Dual<double, grad_array<2>>;
+using my_dual2 = autodiff::forward::Dual<double, Eigen::Array2d>;
 
 using MyVector2 = Eigen::Matrix<my_dual2, 2, 1>;
 
@@ -38,8 +36,8 @@ template<typename Func>
 auto my_jacobian(Func &f, MyVector2 &in)
 {
     // set elements in grad to 0 bzw. 1
-    in(0).grad.set_to_one_all_others_zero<0>();
-    in(1).grad.set_to_one_all_others_zero<1>();
+    in(0).grad = { 0,1 };
+    in(1).grad = { 1,0 };
     
     // compute function
     MyVector2 F = f(in);
@@ -47,21 +45,23 @@ auto my_jacobian(Func &f, MyVector2 &in)
     Eigen::Matrix2d jac;
     
     // Read out values
-    jac(0,0) = F(0).grad.array[0];
-    jac(0,1) = F(0).grad.array[1];
-    jac(1,0) = F(1).grad.array[0];
-    jac(1,1) = F(1).grad.array[1];
+    jac(0,0) = F(0).grad(0);
+    jac(0,1) = F(0).grad(1);
+    jac(1,0) = F(1).grad(0);
+    jac(1,1) = F(1).grad(1);
     
     return jac;
 }
 
-int main(int argc, char **argv) 
+int main() 
 {
     auto f = [](MyVector2 &x) { return MyVector2{ x[0]*x[1], 2*x[0]*x[1] }; };
+    
+    std::cout << "f(x,y) = { xy, 2xy }\n" << std::endl;
     
     MyVector2 in;
     in(0) = 5.0;
     in(1) = 2.0;
     
-    std::cout << "j = \n" << my_jacobian(f, in) << std::endl;
+    std::cout << "j at (" << in(0) << ", " << in(1) << ") = \n" << my_jacobian(f, in) << std::endl;
 }
